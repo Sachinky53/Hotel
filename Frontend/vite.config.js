0838db04aa8e3d9d7ccdcfd795a1d6dc7e4c832c
Sachinky53 <120941174+Sachinky53@users.noreply.github.com>
2025-08-15 16:52:56 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/', // ← Add this line if not there
   plugins: [
     react(),
     tailwindcss(),
